aa84924377fde8589b309a7af44a1be1634323bc
kenstott <128912107+kenstott@users.noreply.github.com>
2026-08-10 20:09:38 -0400

    chore: canary stamp

--- provisa/api/jsonapi/naming.py
+++ provisa/api/jsonapi/naming.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2026 Kenneth Stott
+# Canary: 8fcae53e-8f0e-496d-941b-a04721eae752
 #
 # This source code is licensed under the Business Source License 1.1
 # found in the LICENSE file in the root directory of this source tree.
